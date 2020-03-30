2d979c415dce4a38e949b77b44d97b7b77236d8c
Paul Martin <paul.martin91@hotmail.co.uk>
2020-03-30 13:11:44 +0100

    added front-end and back-end

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const serverAddress = 'http://localhost:3001'
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
       value: "",
-      test: ""
+      instance: "",
+      listModules: [],
+      ids: []
     }
     this.handleClick = this.handleClick.bind(this)
     this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount(){
+    fetch(`${serverAddress}/api/getlist`).then( async res=>{
+      console.log('hit')
+     let array = await res.json()
+     //console.log(array)
+     return array
+    }).then( async res=>{
+      let list = await {}
+      list.urls = await Object.keys(res).map(x=>res[x].url)
+      list.id = await Object.keys(res).map(x=>res[x]._id)
+     // console.log(list)
+      return list
+    }).then(res=>{
+      this.setState({
+        listModules: res.urls,
+        ids: res.id
+      })
+    })
   }
 
   handleChange(event){
@@ -19,28 +44,62 @@ class App extends React.Component{
   }
 
   handleClick(event){
-    console.log(event.target.value)
     let urlToShortern = event.target.value
-    fetch('http://localhost:3001/new - '+urlToShortern) //new/' + this.state.value)
-    .then(res=>{
-      let toString = res.text()
-      return toString
-    })
-    .then(data=>{
+    console.log(urlToShortern)
+    fetch(`${serverAddress}/new%20-%20${urlToShortern}`, 
+      {
+        method: 'get', 
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      ) //new/' + this.state.value)
+      .then( async data=>{
+      let dataJSON = await data.json()
+      // let urls = Object.keys(dataJSON.fullList).map(x=>dataJSON.fullList[x].url)
+      let urls = await Object.keys(dataJSON.fullList).map(x=>dataJSON.fullList[x].url)
+      let id = await Object.keys(dataJSON.fullList).map(x=>dataJSON.fullList[x]._id)
       this.setState({
-        test: data
+          resUrl: 'http://localhost:3001/'+data.link,
+          listModules: urls,
+          instance: data.instance,
+          ids: id
+        })
       })
-    })
   }
 
   render(){
     return(
       <div>
-        <h1>URL Shortening microservice</h1>
-        <p>On this site you can add in a URL and our server will shortern it for you.</p>
-          <input placeholder = 'Enter URL here...' value = {this.state.value} onChange={this.handleChange} />
-          <button value={this.state.value} onClick={this.handleClick}/>
-          <p>{this.state.test}</p>
+        <div className="jumbotron text-center" style={{"marginBottom": 50}}>
+          <h1>URL Shortening microservice</h1>
+          <p>On this site you can add in a URL and our server will shortern it for you.</p>
+        </div>
+        <div className="container" >
+        <div className="input-group w-50">
+          <input type="text" className="form-control input-sm" placeholder = 'Enter URL here...' value = {this.state.value} onChange={this.handleChange} />
+          <button type="button" className="btn btn-success" style={{"marginLeft": "5px"}} value={this.state.value} onClick={this.handleClick}>Shortern URL</button>
+        </div>
+        <br />
+            <h1>Url List<span style={{"marginLeft": "25px"}} className="badge badge-pill badge-primary" >{this.state.ids.length}</span></h1>
+            {this.state.ids.length === 0 ? 
+              <p>No URLs in list</p> 
+              : 
+              this.state.ids.map((x, y)=>{
+                return (
+                  <a href={`${serverAddress}/${x}`} className="list-group-item" href = {`${serverAddress}/${x}`}>
+                  <h4 className="list-group-item-heading">{`${serverAddress}/${x}`}</h4>
+                  <p className="list-group-item-text">{this.state.listModules[y]}</p>
+                  </a>
+                )
+              })
+            }
+          </div>
+        <footer class="py-4 bg-light" style={{"marginTop": 50}}>
+          <div class="footer-copyright text-center py-3">
+            <p>Coded by <a href="https://paulmartin91.github.io/">Paul Martin</a></p>
+          </div>
+        </footer>
       </div>
     )
   }
--- server/app.js
+++ server/app.js
@@ -1,47 +1,183 @@
-var express = require('express')
-var app = express()
-var cors = require('cors')
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 var dns = require('dns')
 
-var urlList = {
+var cors = require('cors');
 
-}
+var app = express();
 
-app.use(cors())
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+app.use('/public', express.static(process.cwd() + '/public'));
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.DB_URI);
 
-    app.get('/new%20-%20:urlToShortern?', (req, res, next)=>{
-        //get params
-        //console.log(1)
-        req.urlToShortern = req.params.urlToShortern
-        next();
-    }, (req, res, next)=>{
-        //check if valid url
-        console.log(req.urlToShortern)
-            dns.lookup(req.urlToShortern, (err, address, family) => {
-                if(err) {
-                    res.send('invalid')
-                    return
-                } else next()
-            });
-    }, (req, res, next)=>{
-        //check if the url is already in our object?
-        if (Object.keys(urlList).length == 0) {
-            res.send({'original_url:': req.urlToShortern, 'link': 1})
-            urlList[1] = req.urlToShortern
-        } else {
-            //console.log(Object.keys(urlList))
-            Object.keys(urlList).some(x=>{
-                if (urlList[x] == req.urlToShortern) res.send(JSON.parse({'original_url:': req.urlToShortern, 'link': x}))
-            })
-        }
-        next()
-        //Object.keys(urlList).forEach(x=>console.log)
+app.use(cors());
+
+//uncomment below to connect to mongodb
+//process.env.MONGO_URI = [add in MongoDB URI]
+
+//uncomment below to connect locally
+process.env.MONGO_URI = 'mongodb://localhost:27017'
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client)=>{
+  if (err){console.log(err)} else {console.log('mongo db connected')}
+  return;
+});
+
+
+var urlSchema = new mongoose.Schema({
+  _id: Number,
+  url: String,
+});
+
+var Urls = mongoose.model("urls", urlSchema);
+
+//get full list
+app.get('/api/getlist', async (req, res, done) => {
+    const getList = await Urls.find({}, async (err, fullList, done) => {
+        const addList = req.fullList = await fullList
+    })
+    done()
+}, (req, res)=>{
+  res.send(req.fullList)
+})
+
+//delete all instances of a specific URL
+app.get("/api/removeAll:urltodelete?", async (req, res) => {
+
+  //delete all in query
+  const del = await Urls.deleteMany({/* url: req.params.urltodelete  <- leave this blank to delete all */ }, (err, res) => {
+    if (err) {
+      console.log(err)
+      res.send(`no matching url's on record`)
+      done()
     }
-    // , (req, res, next)=>{
-    //     //res.send('fin')
-    // }
-    )
+    console.log(res)
+  });
 
-var listener = app.listen(3001, function(){
-    console.log('working on port 3000')
+  //check all are deleted
+  const results = await Urls.findOne({ url: req.params.urltodelete });
+
+  //return result
+  results == null ? res.send('all deleted') : res.send(`error ${results}`)
+});
+
+
+//get new url request
+app.get('/new%20-%20:urlToShortern(*)', async (req, res, done) => {
+
+  const fulllist = await Urls.find({}, async (err, fullList) => {
+    if (err) console.log(err)
+    req.fullList = await fullList
+  })
+
+  //save params
+  req.urlToShortern = await req.params.urlToShortern;
+  
+  //check if valid DNS
+  dns.lookup(req.urlToShortern, async (err, address, family) => {
+    if(err || address == '92.242.132.24') {
+      //if invalid, send message
+      console.log(`invalid dns - error ${err}`)
+      res.send('invalid DNS')
+    } else {
+      console.log(`valid dns at ${address}` )
+      req.validDNS = await true
+      done()
+    }
+  })
+  
+}, async (req, res, done)=>{
+  
+  //check if url already exists
+  const findURL = await Urls.findOne({url: req.urlToShortern}, async (err, instance) => {
+    if (err) console.log(err)
+    
+    //if it doesn't...
+    if (instance == null) {
+    
+    //count number of instances
+    req.number = await parseInt(req.fullList[req.fullList.length-1].id)+1
+    console.log(req.number)
+
+    //create new instance with param as url and n instances+1 as _id
+    const createNew = await Urls.create({url: req.urlToShortern,  _id: req.number}, async (err, instance)=>{
+      if (err) console.log(err)
+      
+      //return new instance
+      req.instance = await instance
+
+      //add new instance
+      const addInstance = await req.fullList.push(req.instance)
+      
+      done();  
+    });
+    
+    } else {
+      //return existing instance
+      req.instance = await 'existing instance'
+      console.log('existing instance')
+      done();
+    }
+  })
+
+  
+}, async (req, res)=>{
+
+  let resultObj = await {}
+
+  //add full list of urls to response
+  resultObj.fullList = await req.fullList
+  
+  //add instance to response
+  resultObj.instance = await req.instance
+  const log = await console.log(`full list = ${resultObj.fullList}`)
+  res.send(resultObj)
+  return;
+})
+
+
+//handle url number requests
+app.get('/:short(*)', async (req, res) => {
+  
+  console.log('number request')
+  
+  //check if valid url number
+  if (/^\d+$/.test(req.params.short)){
+    console.log(req.params.short)
+    
+    //save params
+    var num = await req.params.short
+  
+    //check if number exists
+    const findInstance = await Urls.findById(num, (err, data) => {
+      if (err) console.log(err)
+      
+      //if it doesn't send message
+      if (data == null) {
+        console.log('number not found')
+        res.send('number not on record')
+      } else {
+        console.log(`number found - ${data.url}`)
+        res.redirect(`https://${data.url}`)
+      }
+    })
+  } else {
+    res.send(`invalid url`)
+  }
+})
+  
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/', (req, res)=> console.log(`3`))
+
+app.listen(3001, function () {
+  console.log('Node.js listening ...');
 });
